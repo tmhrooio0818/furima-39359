10611c825b9f12be7cd6d9db66c238dd36bbdd9c
tmhrooio0818 <tmhrooio0818@gmail.com>
2023-09-24 01:13:05 +0900

    商品出品機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,34 @@
 class ItemsController < ApplicationController
+
+  # ログインしていないユーザーはログインページに促す
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
+    @items = Item.includes(:user)
   end
 
-  
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :title, :detail, :category_id, :quality_id, :shipping_fee_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
 end
